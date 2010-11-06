a0da7fe5cec7cf6f75e1ab78c66c6985615d8ad9
aureliojargas <aureliojargas@8350de35-f252-0410-a59d-9bf392a7b71e>
2010-11-06 22:31:23 +0000

    Adding 'dist' folder, with the tools used to generate the .tgz package for a new txt2tags version. The folder also have the txt2tags PAD file and the Freshmeat site contents.

---
+++ dist/check-code.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# check-code.sh - Checks txt2tags code
+# by Aurelio Jargas
+
+t2t=../txt2tags
+
+hecho(){ echo -e "\033[36;1mCode: $*\033[m"; }
+
+cd $(dirname "$0")
+
+hecho 'checking shebang'
+[ "$(head -1 $t2t)" != "#!/usr/bin/env python" ] && echo FAIL
+
+hecho "checking if executable"
+ls -l $t2t | grep -qs '^-rwxr-xr-x' || echo FAIL
+
+hecho "checking year of Copyright"
+grep -qs "Copyright.*$(date +%Y)" $t2t || echo FAIL
+
+hecho "searching for !ASCII chars"
+grep -v '^[	-~]*$' $t2t  # [TAB-~]
+
+hecho "searching for useless blanks"
+egrep '[^ 	][ 	]+$' $t2t | sed -n l  # \S\s$
+egrep ' 	' $t2t | sed -n l             # space+TAB
+# grep '^ ' $t2t                                # leading space
+
+hecho "searching for lines >80 columns"
+# sed "s/$(echo -ne '\t')/        /g" $t2t | egrep '.{81}'
+echo DISABLED
+
+hecho 'checking version'
+$t2t -V | sed 's/<.*//'
+
