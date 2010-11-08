8a3a7be9fecb885ee58be4741d171bb211f75a75
Alexis Metaireau <ametaireau@gmail.com>
2010-11-08 23:30:57 +0000

    added basic setup.py for packaging.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "txt2tags",
+    version = '2.6',
+    url = 'http://code.google.com/p/txt2tags/',
+    author = 'Aurelio Verde',
+    author_email = 'verde@aurelio.net',
+    description = "Document generator. Reads a text file with minimal markup as **bold** and //italic// and converts it to various formats",
+    long_description=open('README').read(),
+    include_package_data = True,
+    scripts = ['txt2tags',],
+)
