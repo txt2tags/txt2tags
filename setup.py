3c4c0e293093cd416d3c239cd3da140a72d35132
aureliojargas <aureliojargas@8350de35-f252-0410-a59d-9bf392a7b71e>
2010-11-09 04:12:47 +0000

    setup: Updating url, author.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name = "txt2tags",
     version = '2.6',
-    url = 'http://code.google.com/p/txt2tags/',
-    author = 'Aurelio Verde',
+    url = 'http://txt2tags.org',
+    author = 'Aurelio Jargas',
     author_email = 'verde@aurelio.net',
     description = "Document generator. Reads a text file with minimal markup as **bold** and //italic// and converts it to various formats",
     long_description=open('README').read(),
