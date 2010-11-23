11e5f7244393f519719d21dc37517f38c223326d
aureliojargas <aureliojargas@8350de35-f252-0410-a59d-9bf392a7b71e>
2010-11-23 18:05:54 +0000

    test: http: Added new tests.

--- trunk/test/http/run.py
+++ trunk/test/http/run.py
@@ -14,30 +14,40 @@ lib.OK = lib.FAILED = 0
 lib.ERROR_FILES = []
 
 remote_root = 'http://txt2tags.org/test/'
-remote_infiles = ['mtime.t2t','not-found.t2t']
+remote_infiles = [
+	'mtime.t2t',
+	'not-found.t2t',
+	]
+remote_mapping = {
+	'remote-outfile': 'simple.t2t',
+	'stdout': 'simple.t2t',
+	}
 
 def run():
 	# test all OK files found
 	for outfile in glob.glob("ok/*"):
-		stderr = 0
 		basename = re.sub('\..*?$', '', outfile.replace('ok/', ''))
 		target = re.sub('.*\.', '', outfile)
-		if target == 'out':
-			target = 'txt'
-			stderr = 1
 		infile = basename + ".t2t"
 		if infile in remote_infiles:
 			infile = remote_root + infile
+		if basename in remote_mapping:
+			infile = remote_root + remote_mapping[basename]
 		outfile = outfile.replace('ok/', '')
 		if lib.initTest(basename, infile, outfile):
 			cmdline = []
 			cmdline.extend(['-i', infile])
-			if infile.startswith(remote_root):
+			if infile.startswith(remote_root) \
+				and basename != 'remote-outfile':
 				cmdline.extend(['-o', outfile])
-			if stderr:
-				cmdline.extend(['-o', '-'])
+			if basename == 'not-found':
 				cmdline.append('>' + outfile)
 				cmdline.append('2>&1')
+			elif basename == 'stdout':
+				cmdline.extend(['-o', '-'])
+				cmdline.append('>' + outfile)
+			elif basename == 'remote-outfile':
+				cmdline.append('2>' + outfile)
 			lib.convert(cmdline)
 			lib.diff(outfile)
 	# clean up
