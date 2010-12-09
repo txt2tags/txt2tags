1a0c660830cdbd79853e65d438205a1aad2669bc
aureliojargas <aureliojargas@8350de35-f252-0410-a59d-9bf392a7b71e>
2010-12-09 18:24:00 +0000

    po/tools/stats.sh: Dumps results to STDOUT instead ../stats.txt

--- po/tools/stats.sh
+++ po/tools/stats.sh
@@ -2,17 +2,13 @@
 # 2010-06-18
 #
 
-output=stats.txt
-
 cd $(dirname "$0")
 cd ..
 for po in *.po
 do
 	printf "%10s" "$po: "
-	LANG=C msgfmt -c --statistics $po 2>&1
-done > $output
+	LANG=C msgfmt --statistics $po 2>&1
+done
 
 # remove temporary files
 rm -f messages messages.mo
-
-echo Saved $output
