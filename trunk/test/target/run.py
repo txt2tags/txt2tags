67f515923c0b30afbdcb3706bb69fdfa19e39662
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2011-07-10 03:39:33 +0000

    Rst: no border txt2tags tables now generate simple rst tables.

--- trunk/test/target/run.py
+++ trunk/test/target/run.py
@@ -13,9 +13,9 @@ del sys.path[0]
 lib.OK = lib.FAILED = 0
 lib.ERROR_FILES = []
 
-all_targets = 'aap aas aat aaw adoc bbcode creole dbk doku gwiki html html5 lout man md mgp moin pm6 pmw ods red rtf sgml spip tex txt txt2t wiki xhtml xhtmls'.split()
+all_targets = 'aap aas aat aaw adoc bbcode creole dbk doku gwiki html html5 lout man md mgp moin pm6 pmw ods red rst rtf sgml spip tex txt txt2t wiki xhtml xhtmls'.split()
 
-tableable = 'aat aas aaw creole doku gwiki html html5 man md moin pmw ods red rtf sgml spip tex wiki xhtml xhtmls'.split()
+tableable = 'aat aas aaw creole doku gwiki html html5 man md moin pmw ods red rst rtf sgml spip tex wiki xhtml xhtmls'.split()
 
 def run():
     # test all .t2t files found
