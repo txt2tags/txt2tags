6eae86caac56d551f87addea4903a0f5c00f19c1
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-05-28 14:33:29 +0000

    TARGETS: adds wiki, gwiki, pmw, tml and doku targets.

---
+++ targets/doku.py
@@ -0,0 +1,88 @@
+"""
+A DokuWiki target.
+http://www.dokuwiki.org
+"""
+
+NAME = 'DokuWiki page'
+
+TYPE = 'wiki'
+
+HEADER = """\
+===== %(HEADER1)s =====
+
+**//%(HEADER2)s//**
+
+//%(HEADER3)s//
+"""
+
+# http://www.dokuwiki.org/syntax
+# http://www.dokuwiki.org/playground:playground
+# Hint: <br> is \\ $
+# Hint: You can add footnotes ((This is a footnote))
+TAGS = {
+    'title1'               : '===== \a =====',
+    'title2'               : '==== \a ===='  ,
+    'title3'               : '=== \a ==='    ,
+    'title4'               : '== \a =='      ,
+    'title5'               : '= \a ='        ,
+    # DokuWiki uses '  ' identation to mark verb blocks (see indentverbblock)
+    'blockQuoteLine'       : '>'             ,
+    'fontMonoOpen'         : "''"            ,
+    'fontMonoClose'        : "''"            ,
+    'fontBoldOpen'         : "**"            ,
+    'fontBoldClose'        : "**"            ,
+    'fontItalicOpen'       : "//"            ,
+    'fontItalicClose'      : "//"            ,
+    'fontUnderlineOpen'    : "__"            ,
+    'fontUnderlineClose'   : "__"            ,
+    'fontStrikeOpen'       : '<del>'         ,
+    'fontStrikeClose'      : '</del>'        ,
+    'listItemOpen'         : '  * '          ,
+    'numlistItemOpen'      : '  - '          ,
+    'bar1'                 : '----'          ,
+    'url'                  : '[[\a]]'        ,
+    'urlMark'              : '[[\a|\a]]'     ,
+    'email'                : '[[\a]]'        ,
+    'emailMark'            : '[[\a|\a]]'     ,
+    'img'                  : '{{\a}}'        ,
+    'imgAlignLeft'         : '{{\a }}'       ,
+    'imgAlignRight'        : '{{ \a}}'       ,
+    'imgAlignCenter'       : '{{ \a }}'      ,
+    'tableTitleRowOpen'    : '^ '            ,
+    'tableTitleRowClose'   : ' ^'            ,
+    'tableTitleCellSep'    : ' ^ '           ,
+    'tableRowOpen'         : '| '            ,
+    'tableRowClose'        : ' |'            ,
+    'tableCellSep'         : ' | '           ,
+    # DokuWiki has no attributes. The content must be aligned!
+    # '_tableCellAlignRight' : '<)>'           ,  # ??
+    # '_tableCellAlignCenter': '<:>'           ,  # ??
+    # DokuWiki colspan is the same as txt2tags' with multiple |||
+    # 'comment'             : '## \a'         ,  # ??
+    # TOC is automatic
+}
+
+RULES = {
+    'indentverbblock': 1,  # DokuWiki uses '  ' to mark verb blocks
+    'spacedlistitem': 1,
+    'linkable': 1,
+    'keeplistindent': 1,
+    'tableable': 1,
+    'barinsidequote': 1,
+    'tablecellstrip': 1,
+    'autotocwithbars': 1,
+    'autonumberlist': 1,
+    'imgalignable': 1,
+    'tablecellaligntype': 'cell',
+
+    'blanksaroundpara': 1,
+    'blanksaroundverb': 1,
+    # 'blanksaroundquote': 1,
+    'blanksaroundlist': 1,
+    'blanksaroundnumlist': 1,
+    'blanksarounddeflist': 1,
+    'blanksaroundtable': 1,
+    'blanksaroundbar': 1,
+    'blanksaroundtitle': 1,
+    'blanksaroundnumtitle': 1,
+}
---
+++ targets/gwiki.py
@@ -0,0 +1,66 @@
+"""
+A Google Wiki target.
+"""
+
+NAME = 'Google Wiki page'
+
+TARGET = 'wiki'
+
+HEADER = """\
+*%(HEADER1)s*
+
+%(HEADER2)s
+
+_%(HEADER3)s_
+"""
+
+# http://code.google.com/p/support/wiki/WikiSyntax
+TAGS = {
+    'title1'               : '= \a ='        ,
+    'title2'               : '== \a =='      ,
+    'title3'               : '=== \a ==='    ,
+    'title4'               : '==== \a ===='  ,
+    'title5'               : '===== \a =====',
+    'blockVerbOpen'        : '{{{'           ,
+    'blockVerbClose'       : '}}}'           ,
+    'blockQuoteLine'       : '  '            ,
+    'fontMonoOpen'         : '{{{'           ,
+    'fontMonoClose'        : '}}}'           ,
+    'fontBoldOpen'         : '*'             ,
+    'fontBoldClose'        : '*'             ,
+    'fontItalicOpen'       : '_'             ,  # underline == italic
+    'fontItalicClose'      : '_'             ,
+    'fontStrikeOpen'       : '~~'            ,
+    'fontStrikeClose'      : '~~'            ,
+    'listItemOpen'         : ' * '           ,
+    'numlistItemOpen'      : ' # '           ,
+    'url'                  : '\a'            ,
+    'urlMark'              : '[\a \a]'       ,
+    'email'                : 'mailto:\a'     ,
+    'emailMark'            : '[mailto:\a \a]',
+    'img'                  : '[\a]'          ,
+    'tableRowOpen'         : '|| '           ,
+    'tableRowClose'        : ' ||'           ,
+    'tableCellSep'         : ' || '          ,
+}
+
+RULES = {
+    'spacedlistitem': 1,
+    'linkable': 1,
+    'keeplistindent': 1,
+    'tableable': 1,
+    'tabletitlerowinbold': 1,
+    'tablecellstrip': 1,
+    'autonumberlist': 1,
+
+    'blanksaroundpara': 1,
+    'blanksaroundverb': 1,
+    # 'blanksaroundquote': 1,
+    'blanksaroundlist': 1,
+    'blanksaroundnumlist': 1,
+    'blanksarounddeflist': 1,
+    'blanksaroundtable': 1,
+    # 'blanksaroundbar': 1,
+    'blanksaroundtitle': 1,
+    'blanksaroundnumtitle': 1,
+}
