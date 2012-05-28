e25934083fb8cb9c7a957e8045ef89f6085afe25
Florent Gallaire <fgallaire@gmail.com>
2012-05-28 14:33:29 +0000

    TARGETS: adds wiki, gwiki, pmw, tml and doku targets.

--- targets/null.py
+++ targets/null.py
@@ -2,10 +2,10 @@
 A do-nothing target, uses the default values.
 """
 
-# NAME default to 'Null target'
+# NAME defaults to 'Null target'
 #NAME =
 
-# TYPE default to 'others', possible values are 'wiki', 'html', 'office' and 'text'
+# TYPE defaults to 'others', possible values are 'wiki', 'html', 'office' and 'text'
 #TYPE =
 
 # HEADER defaults to ''
