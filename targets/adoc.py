268d4680e113d1c656cfc062e3d7ac2305334cbf
Florent Gallaire <fgallaire@gmail.com>
2012-05-28 22:57:07 +0000

    TARGETS: adds mom, moin, adoc and txt2t targets.

---
+++ targets/adoc.py
@@ -0,0 +1,56 @@
+"""
+An AsciiDoc target.
+http://www.methods.co.nz/asciidoc
+"""
+
+NAME = 'AsciiDoc document'
+
+TYPE = 'wiki'
+
+HEADER = """\
+= %(HEADER1)s
+%(HEADER2)s
+%(HEADER3)s
+"""
+
+# http://powerman.name/doc/asciidoc
+TAGS = {
+    'title1'               : '== \a'         ,
+    'title2'               : '=== \a'        ,
+    'title3'               : '==== \a'       ,
+    'title4'               : '===== \a'      ,
+    'title5'               : '===== \a'      ,
+    'blockVerbOpen'        : '----'          ,
+    'blockVerbClose'       : '----'          ,
+    'fontMonoOpen'         : '+'             ,
+    'fontMonoClose'        : '+'             ,
+    'fontBoldOpen'         : '*'             ,
+    'fontBoldClose'        : '*'             ,
+    'fontItalicOpen'       : '_'             ,
+    'fontItalicClose'      : '_'             ,
+    'listItemOpen'         : '- '            ,
+    'listItemLine'         : '\t'            ,
+    'numlistItemOpen'      : '. '            ,
+    'url'                  : '\a'            ,
+    'urlMark'              : '\a[\a]'        ,
+    'email'                : 'mailto:\a'     ,
+    'emailMark'            : 'mailto:\a[\a]' ,
+    'img'                  : 'image::\a[]'   ,
+}
+
+RULES = {
+    'spacedlistitem': 1,
+    'linkable': 1,
+    'keeplistindent': 1,
+    'autonumberlist': 1,
+    'autonumbertitle': 1,
+    'listnotnested': 1,
+    'blanksaroundpara': 1,
+    'blanksaroundverb': 1,
+    'blanksaroundlist': 1,
+    'blanksaroundnumlist': 1,
+    'blanksarounddeflist': 1,
+    'blanksaroundtable': 1,
+    'blanksaroundtitle': 1,
+    'blanksaroundnumtitle': 1,
+}
