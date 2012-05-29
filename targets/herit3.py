9ec39370788656778e2b9251c1bd36760b443c61
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-05-29 22:27:51 +0000

    TARGETS: adds two more herit targets.

---
+++ targets/herit3.py
@@ -0,0 +1,17 @@
+"""
+The minimal target which inherits from the mark target.
+"""
+
+# import of the base target
+from mark import *
+
+# NAME inherits from mark.NAME
+NAME = 'Minimal inheriting target'
+
+# TYPE inherits from mark.TYPE
+
+# HEADER inherits from mark.HEADER
+
+# TAGS inherits from  mark.TAGS
+
+# RULES inherits from mark.RULES
