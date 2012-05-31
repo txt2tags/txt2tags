98c77d7165fc7c9c9cfd2e61f34ced302068bf73
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-05-31 03:18:33 +0000

    TARGETS: adds one more herit target.

--- trunk/targets/config.py
+++ trunk/targets/config.py
@@ -3,4 +3,4 @@ Copy your targets in this directory, and add them to the TARGETS_LIST list.
 """
 
 TARGETS_LIST = []
-#TARGETS_LIST = ['mark', 'herit', 'herit2', 'herit3', 'mama', 'null']
+#TARGETS_LIST = ['mark', 'herit', 'herit2', 'herit3', 'herit4', 'mama', 'null']
---
+++ trunk/targets/herit4.py
@@ -0,0 +1,16 @@
+"""
+The other minimal target which inherits from the mark target.
+"""
+
+# import of the base target
+from mark import TYPE, HEADER, TAGS, RULES
+
+# NAME defaults to 'Herit4 target'
+
+# TYPE inherits from mark.TYPE
+
+# HEADER inherits from mark.HEADER
+
+# TAGS inherits from  mark.TAGS
+
+# RULES inherits from mark.RULES
