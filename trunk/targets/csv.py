21bcde090375adda25b683a7a6b1fd94a2ee5821
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-06-03 01:03:16 +0000

    TARGETS: adds csv and ods targets.

---
+++ trunk/targets/csv.py
@@ -0,0 +1,13 @@
+"""
+A CSV spreadsheet target.
+Target specific occurrence number in txt2tags core: 1.
+"""
+
+NAME = 'CSV spreadsheet'
+
+TYPE = 'office'
+
+RULES = {
+    'tableable': 1,
+    'tableonly': 1,
+}
