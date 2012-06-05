0cd17c464229d38e2c3b3f3fb6e7571dea056cfa
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-06-05 23:59:28 +0000

    TARGETS: adds pm6, sgml and lout targets.

--- targets/config.py
+++ targets/config.py
@@ -3,4 +3,9 @@ Copy your targets in this directory, and add them to the TARGETS_LIST list.
 """
 
 TARGETS_LIST = []
+
+# For txt2tags test
 #TARGETS_LIST = ['mark', 'herit', 'herit2', 'herit3', 'herit4', 'mama', 'null']
+
+# For txt2tagslite use
+#TARGETS_LIST = ['adoc', 'csv', 'doku', 'moin', 'pm6', 'red', 'spip', 'txt2t', 'bbcode', 'creole', 'dbk', 'gwiki', 'lout', 'md', 'mom', 'ods', 'pmw', 'sgml', 'tml', 'wiki']
