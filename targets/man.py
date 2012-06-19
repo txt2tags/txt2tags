25264ac3f13edf7fb3745bad42b0083c4c20d1e2
Florent Gallaire <fgallaire@gmail.com>
2012-06-19 02:14:22 +0000

    TARGETS: adds man, mgp and rtf targets.

---
+++ targets/man.py
@@ -0,0 +1,75 @@
+"""
+A Man target.
+Target specific occurrence number in txt2tags core: 3.
+"""
+
+NAME = 'UNIX Manual page'
+
+TYPE = 'text'
+
+HEADER = """\
+.TH "%(HEADER1)s" 1 "%(HEADER3)s" "%(HEADER2)s"
+"""
+
+# man groff_man ; man 7 groff
+TAGS = {
+    'paragraphOpen'         : '.P'     ,
+    'title1'                : '.SH \a' ,
+    'title2'                : '.SS \a' ,
+    'title3'                : '.SS \a' ,
+    'title4'                : '.SS \a' ,
+    'title5'                : '.SS \a' ,
+    'blockVerbOpen'         : '.nf'    ,
+    'blockVerbClose'        : '.fi\n'  ,
+    'blockQuoteOpen'        : '.RS'    ,
+    'blockQuoteClose'       : '.RE'    ,
+    'fontBoldOpen'          : '\\fB'   ,
+    'fontBoldClose'         : '\\fR'   ,
+    'fontItalicOpen'        : '\\fI'   ,
+    'fontItalicClose'       : '\\fR'   ,
+    'listOpen'              : '.RS'    ,
+    'listItemOpen'          : '.IP \(bu 3\n',
+    'listClose'             : '.RE'    ,
+    'numlistOpen'           : '.RS'    ,
+    'numlistItemOpen'       : '.IP \a. 3\n',
+    'numlistClose'          : '.RE'    ,
+    'deflistItem1Open'      : '.TP\n'  ,
+    'bar1'                  : '\n\n'   ,
+    'url'                   : '\a'     ,
+    'urlMark'               : '\a (\a)',
+    'email'                 : '\a'     ,
+    'emailMark'             : '\a (\a)',
+    'img'                   : '\a'     ,
+    'tableOpen'             : '.TS\n~A~~B~tab(^); ~C~.',
+    'tableClose'            : '.TE'     ,
+    'tableRowOpen'          : ' '       ,
+    'tableCellSep'          : '^'       ,
+    '_tableAlignCenter'     : 'center, ',
+    '_tableBorder'          : 'allbox, ',
+    '_tableColAlignLeft'    : 'l'       ,
+    '_tableColAlignRight'   : 'r'       ,
+    '_tableColAlignCenter'  : 'c'       ,
+    'comment'               : '.\\" \a'
+}
+
+RULES = {
+    'spacedlistitem': 1,
+    'tagnotindentable': 1,
+    'tableable': 1,
+    'tablecellaligntype': 'column',
+    'tabletitlerowinbold': 1,
+    'tablecellstrip': 1,
+    'barinsidequote': 1,
+    'parainsidelist': 0,
+
+    'blanksaroundpara': 1,
+    'blanksaroundverb': 1,
+    # 'blanksaroundquote': 1,
+    'blanksaroundlist': 1,
+    'blanksaroundnumlist': 1,
+    'blanksarounddeflist': 1,
+    'blanksaroundtable': 1,
+    # 'blanksaroundbar': 1,
+    'blanksaroundtitle': 1,
+    'blanksaroundnumtitle': 1,
+}
---
+++ targets/mgp.py
@@ -0,0 +1,105 @@
+"""
+A MagicPoint target.
+http://member.wide.ad.jp/wg/mgp
+Target specific occurrence number in txt2tags core: 5.
+"""
+
+NAME = 'MagicPoint presentation'
+
+TYPE = 'office'
+
+HEADER = """\
+#!/usr/X11R6/bin/mgp -t 90
+%%deffont "normal"    xfont  "utopia-medium-r", charset "iso8859-1"
+%%deffont "normal-i"  xfont  "utopia-medium-i", charset "iso8859-1"
+%%deffont "normal-b"  xfont  "utopia-bold-r"  , charset "iso8859-1"
+%%deffont "normal-bi" xfont  "utopia-bold-i"  , charset "iso8859-1"
+%%deffont "mono"      xfont "courier-medium-r", charset "iso8859-1"
+%%default 1 size 5
+%%default 2 size 8, fore "yellow", font "normal-b", center
+%%default 3 size 5, fore "white",  font "normal", left, prefix "  "
+%%tab 1 size 4, vgap 30, prefix "     ", icon arc "red" 40, leftfill
+%%tab 2 prefix "            ", icon arc "orange" 40, leftfill
+%%tab 3 prefix "                   ", icon arc "brown" 40, leftfill
+%%tab 4 prefix "                          ", icon arc "darkmagenta" 40, leftfill
+%%tab 5 prefix "                                ", icon arc "magenta" 40, leftfill
+%%%%------------------------- end of headers -----------------------------
+%%page
+
+
+
+
+
+%%size 10, center, fore "yellow"
+%(HEADER1)s
+
+%%font "normal-i", size 6, fore "white", center
+%(HEADER2)s
+
+%%font "mono", size 7, center
+%(HEADER3)s
+"""
+
+# http://www.inference.phy.cam.ac.uk/mackay/mgp/SYNTAX
+# http://en.wikipedia.org/wiki/MagicPoint
+TAGS = {
+    'paragraphOpen'         : '%font "normal", size 5'     ,
+    'title1'                : '%page\n\n\a\n'              ,
+    'title2'                : '%page\n\n\a\n'              ,
+    'title3'                : '%page\n\n\a\n'              ,
+    'title4'                : '%page\n\n\a\n'              ,
+    'title5'                : '%page\n\n\a\n'              ,
+    'blockVerbOpen'         : '%font "mono"'               ,
+    'blockVerbClose'        : '%font "normal"'             ,
+    'blockQuoteOpen'        : '%prefix "       "'          ,
+    'blockQuoteClose'       : '%prefix "  "'               ,
+    'fontMonoOpen'          : '\n%cont, font "mono"\n'     ,
+    'fontMonoClose'         : '\n%cont, font "normal"\n'   ,
+    'fontBoldOpen'          : '\n%cont, font "normal-b"\n' ,
+    'fontBoldClose'         : '\n%cont, font "normal"\n'   ,
+    'fontItalicOpen'        : '\n%cont, font "normal-i"\n' ,
+    'fontItalicClose'       : '\n%cont, font "normal"\n'   ,
+    'fontUnderlineOpen'     : '\n%cont, fore "cyan"\n'     ,
+    'fontUnderlineClose'    : '\n%cont, fore "white"\n'    ,
+    'listItemLine'          : '\t'                         ,
+    'numlistItemLine'       : '\t'                         ,
+    'numlistItemOpen'       : '\a. '                       ,
+    'deflistItem1Open'      : '\t\n%cont, font "normal-b"\n',
+    'deflistItem1Close'     : '\n%cont, font "normal"\n'   ,
+    'bar1'                  : '%bar "white" 5'             ,
+    'bar2'                  : '%pause'                     ,
+    'url'                   : '\n%cont, fore "cyan"\n\a'    +\
+                              '\n%cont, fore "white"\n'    ,
+    'urlMark'               : '\a \n%cont, fore "cyan"\n\a' +\
+                              '\n%cont, fore "white"\n'    ,
+    'email'                 : '\n%cont, fore "cyan"\n\a'    +\
+                              '\n%cont, fore "white"\n'    ,
+    'emailMark'             : '\a \n%cont, fore "cyan"\n\a' +\
+                              '\n%cont, fore "white"\n'    ,
+    'img'                   : '~A~\n%newimage "\a"\n%left\n',
+    '_imgAlignLeft'         : '\n%left'                    ,
+    '_imgAlignRight'        : '\n%right'                   ,
+    '_imgAlignCenter'       : '\n%center'                  ,
+    'comment'               : '%% \a'                      ,
+    'pageBreak'             : '%page\n\n\n'                ,
+    'EOD'                   : '%%EOD'
+}
+
+RULES = {
+    'tagnotindentable': 1,
+    'spacedlistitem': 1,
+    'imgalignable': 1,
+    'autotocnewpagebefore': 1,
+
+    'blanksaroundpara': 1,
+    'blanksaroundverb': 1,
+    # 'blanksaroundquote': 1,
+    'blanksaroundlist': 1,
+    'blanksaroundnumlist': 1,
+    'blanksarounddeflist': 1,
+    'blanksaroundtable': 1,
+    'blanksaroundbar': 1,
+    'tableable': 1,
+    # 'blanksaroundtitle': 1,
+    # 'blanksaroundnumtitle': 1,
+}
