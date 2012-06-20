20d71064b47e2e7cb7f83206e8fb38a3cf64fef9
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-06-20 03:50:36 +0000

    TARGETS: adds tex and texs targets.

---
+++ targets/texs.py
@@ -0,0 +1,24 @@
+"""
+A LaTeX Spreadsheet target.
+Target specific occurrence number in txt2tags core: 7.
+"""
+
+# inherits from the LaTeX target
+from tex import TYPE, TAGS, RULES
+
+NAME = 'LaTeX Spreadsheet'
+
+HEADER = \
+r"""\documentclass{article}
+\usepackage{graphicx}
+\usepackage[urlcolor=black,colorlinks=true]{hyperref}
+\usepackage[%(ENCODING)s]{inputenc}  %% char encoding
+\usepackage{%(STYLE)s}  %% user defined
+
+\begin{document}
+"""
+
+RULES['tableonly'] = 1
+RULES['spread'] = 1
+RULES['spreadgrid'] = 1
+RULES['spreadmarkup'] = 'tex'
