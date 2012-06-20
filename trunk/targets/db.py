7bb4a225c248868413c128792a281f303d8f03b4
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-06-20 04:07:33 +0000

    TARGETS: adds db target.

---
+++ trunk/targets/db.py
@@ -0,0 +1,14 @@
+"""
+A SQLite target.
+http://www.sqlite.org
+Target VERY specific occurrence number in txt2tags core: 4.
+"""
+
+NAME = 'SQLite database'
+
+TYPE = 'office'
+
+RULES = {
+    'tableable': 1,
+    'tableonly': 1,
+}
