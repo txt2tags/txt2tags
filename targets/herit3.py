e400e5ffa213120afe34ca674a312ac3cdab2888
Florent Gallaire <fgallaire@gmail.com>
2012-07-05 17:28:57 +0000

    TARGETS: fix inherit examples.

--- targets/herit3.py
+++ targets/herit3.py
@@ -5,7 +5,7 @@ The minimal target which inherits from the mark target.
 # import of the base target
 from mark import *
 
-# NAME inherits from mark.NAME
+# overrides the NAME inherited from mark.NAME
 NAME = 'Minimal inheriting target'
 
 # TYPE inherits from mark.TYPE
