b0676b6566c516d74059cfd2bcf0df6d1ba3299b
Florent Gallaire <fgallaire@gmail.com>
2012-07-05 15:58:33 +0000

    TARGETS: fix csvs and texs to avoid inheritance conflicts.

--- targets/csvs.py
+++ targets/csvs.py
@@ -3,9 +3,11 @@ A CSV Spreadsheet target.
 """
 
 # inherits from the CSV table target
-from csv import TYPE, TAGS, RULES
+from csv import TYPE, TAGS
+import csv
 
 NAME = 'CSV Spreadsheet'
 
+RULES = csv.RULES.copy()
 RULES['spread'] = 1
 RULES['spreadmarkup'] = 'txt'
