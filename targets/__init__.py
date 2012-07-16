4894e243635a437d419a6af5844ab999068f94f6
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-07-16 23:22:07 +0000

    TARGETS: adds rst target.

--- targets/__init__.py
+++ targets/__init__.py
@@ -1,4 +1,10 @@
 from config import TARGETS_LIST
 
+# ReStructuredText config
+# http://docs.python.org/release/2.7/documenting/rest.html#sections
+RST_KEYS = 'title level1 level2 level3 level4 level5 bar1 bullet'.split()
+RST_VALUES = '#*=-^"--'  # do not edit here, please use --chars
+RST = dict(zip(RST_KEYS, RST_VALUES))
+
 for target in TARGETS_LIST:
     exec('import ' + target)
