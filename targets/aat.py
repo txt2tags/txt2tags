a83ed1393a4c90609011216c63ef7fb74faaf5d3
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-07-19 20:48:58 +0000

    TARGETS: adds aat target.

---
+++ targets/aat.py
@@ -0,0 +1,37 @@
+from lib import aa
+import txt
+import targets
+
+NAME = 'ASCII Art Text'
+
+TYPE = 'text'
+
+TAGS = {
+    'title1'               : '\a'                     ,
+    'title2'               : '\a'                     ,
+    'title3'               : '\a'                     ,
+    'title4'               : '\a'                     ,
+    'title5'               : '\a'                     ,
+    'blockQuoteLine'       : '        '               ,
+    'listItemOpen'         : targets.AA['bullet'] + ' ',
+    'numlistItemOpen'      : '\a. '                   ,
+    'bar1'                 : aa.line(targets.AA['bar1'], targets.CONF['width']),
+    'bar2'                 : aa.line(targets.AA['bar2'], targets.CONF['width']),
+    'url'                  : '\a'                     ,
+    'urlMark'              : '\a[\a]'                 ,
+    'email'                : '\a'                     ,
+    'emailMark'            : '\a[\a]'                 ,
+    'img'                  : '[\a]'                   ,
+    'imgEmbed'             : '\a'                     ,
+    'fontBoldOpen'         : '*'                      ,
+    'fontBoldClose'        : '*'                      ,
+    'fontItalicOpen'       : '/'                      ,
+    'fontItalicClose'      : '/'                      ,
+    'fontUnderlineOpen'    : '_'                      ,
+    'fontUnderlineClose'   : '_'                      ,
+    'fontStrikeOpen'       : '-'                      ,
+    'fontStrikeClose'      : '-'                      ,
+}
+
+RULES = txt.RULES.copy()
+RULES['confdependenttags'] = 1
