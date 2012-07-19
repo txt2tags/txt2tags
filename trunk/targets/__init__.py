dc6676891b50620a1fba98dd41c59859f85eb527
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-07-19 20:48:58 +0000

    TARGETS: adds aat target.

--- trunk/targets/__init__.py
+++ trunk/targets/__init__.py
@@ -1,5 +1,14 @@
 from config import TARGETS_LIST
 
+CONF = {
+    'width': 72,
+}
+
+# ASCII Art config
+AA_KEYS = 'corner border side bar1 bar2 level2 level3 level4 level5 bullet hhead vhead'.split()
+AA_VALUES = '+-|-==-^"-=$'  # do not edit here, please use --chars
+AA = dict(zip(AA_KEYS, AA_VALUES))
+
 # ReStructuredText config
 # http://docs.python.org/release/2.7/documenting/rest.html#sections
 RST_KEYS = 'title level1 level2 level3 level4 level5 bar1 bullet'.split()
