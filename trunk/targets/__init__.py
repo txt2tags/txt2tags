08b0ced0524fa1fda60782501c0f47d851509049
fgallaire <fgallaire@8350de35-f252-0410-a59d-9bf392a7b71e>
2012-07-22 02:55:03 +0000

    TARGETS: organizes conf.

--- trunk/targets/__init__.py
+++ trunk/targets/__init__.py
@@ -1,22 +1,16 @@
-from config import TARGETS_LIST
+from config import TARGETS_LIST, USE_I18N
 
-USE_I18N = 1   # use gettext for i18ned messages?        (default is 1)
 
-# i18n - just use if available
-if USE_I18N:
-    try:
-        import gettext
-        # If your locale dir is different, change it here
-        cat = gettext.Catalog('txt2tags', localedir='/usr/share/locale/')
-        _ = cat.gettext
-    except:
-        _ = lambda x: x
-else:
-    _ = lambda x: x
+################################################################################
+# General sharing
 
 CONF = {
     'width': 72,
 }
+
+
+################################################################################
+# Target specific sharing
 
 # ASCII Art config
 AA_KEYS = 'corner border side bar1 bar2 level2 level3 level4 level5 bullet hhead vhead'.split()
@@ -28,6 +22,25 @@ AA = dict(zip(AA_KEYS, AA_VALUES))
 RST_KEYS = 'title level1 level2 level3 level4 level5 bar1 bullet'.split()
 RST_VALUES = '#*=-^"--'  # do not edit here, please use --chars
 RST = dict(zip(RST_KEYS, RST_VALUES))
+
+
+################################################################################
+# i18n - just use if available
+
+if USE_I18N:
+    try:
+        import gettext
+        # If your locale dir is different, change it here
+        cat = gettext.Catalog('txt2tags', localedir='/usr/share/locale/')
+        _ = cat.gettext
+    except:
+        _ = lambda x: x
+else:
+    _ = lambda x: x
+
+
+################################################################################
+# Targets import
 
 for target in TARGETS_LIST:
     exec('import ' + target)
