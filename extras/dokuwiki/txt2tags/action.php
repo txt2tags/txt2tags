63082008b899ce3640588e9ce0936809eca59979
Eric Forgeot <eforgeot@gmail.com>
2012-12-11 22:01:45 +0000

    [extra] fixed folder name in dokuwiki t2t plugin

--- extras/dokuwiki/txt2tags/action.php
+++ extras/dokuwiki/txt2tags/action.php
@@ -11,7 +11,7 @@ if (!defined('DOKU_INC')) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once DOKU_PLUGIN.'action.php';
 
-class action_plugin_t2t extends DokuWiki_Action_Plugin {
+class action_plugin_txt2tags extends DokuWiki_Action_Plugin {
 
    function register(&$controller) {
       $controller->register_hook('PARSER_WIKITEXT_PREPROCESS',
