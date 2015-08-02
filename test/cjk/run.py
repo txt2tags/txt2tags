b9d02eb4321e1b4a66f7c5556c23f0a5491ce450
Florent Gallaire <fgallaire@gmail.com>
2015-08-02 15:01:04 +0000

    Fix CJK wrapping
    	modified:   lib/aa.py
    	new file:   test/cjk/CJK-10.t2t
    	new file:   test/cjk/CJK-20.t2t
    	new file:   test/cjk/CJK-6.t2t
    	new file:   test/cjk/CJK-9.t2t
    	new file:   test/cjk/CJK.t2t
    	new file:   test/cjk/ok/CJK-10.aat
    	new file:   test/cjk/ok/CJK-20.aat
    	new file:   test/cjk/ok/CJK-6.aat
    	new file:   test/cjk/ok/CJK-9.aat
    	new file:   test/cjk/ok/CJK.aat
    	new file:   test/cjk/run.py
    	modified:   txt2tags
    	modified:   txt2tagslite

---
+++ test/cjk/run.py
@@ -0,0 +1,44 @@
+#
+# txt2tags CJK wrapping tester (http://txt2tags.org)
+# See also: ../run.py ../lib.py
+#
+
+import os, sys, re, glob
+
+sys.path.insert(0, '..')
+import lib
+del sys.path[0]
+
+# sux
+lib.OK = lib.FAILED = 0
+lib.ERROR_FILES = []
+
+def run():
+    # test all OK files found
+    # Note: txt target is to test the table-to-verbatim mapping
+    for outfile in glob.glob("ok/*"):
+        stderr = 0
+        basename = re.sub('\..*?$', '', outfile.replace('ok/', ''))
+        target = re.sub('.*\.', '', outfile)
+        if target == 'out':
+            target = 'txt'
+            stderr = 1
+        infile = basename + ".t2t"
+        outfile = outfile.replace('ok/', '')
+
+        if lib.initTest(basename, infile, outfile):
+            cmdline = ['-t', target]
+            cmdline.extend(['-i', infile])
+            if stderr:
+                cmdline.extend(['-o', '-'])
+                cmdline.append('>' + outfile)
+                cmdline.append('2>&1')
+            lib.test(cmdline, outfile)
+    # clean up
+    if os.path.isfile(lib.CONFIG_FILE):
+        os.remove(lib.CONFIG_FILE)
+
+    return lib.OK, lib.FAILED, lib.ERROR_FILES
+
+if __name__ == '__main__':
+    print lib.MSG_RUN_ALONE
