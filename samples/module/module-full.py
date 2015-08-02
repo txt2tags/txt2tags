0daa153fb38c7ecbb6f23187230aecad87472d6b
Jendrik Seipp <jendrikseipp@web.de>
2015-08-01 22:15:35 -0700

    fix module-full sample code

--- samples/module/module-full.py
+++ samples/module/module-full.py
@@ -15,25 +15,23 @@ setup.setup_paths()
 
 import txt2tags
 
-# Here is the marked text, it must be a list.
-txt = "Header1\nHeader2\nHeader3\n%!target: html\nBody line 1."
-txt = txt.split('\n')
+# The markup must be a list.
+lines = """\
+Header1
+Header2
+Header3
+%!target: html
+Body line 1.""".splitlines()
 
 # Let's do the conversion
 try:
     # First we parse the text, splitting parts and getting config.
-    data = txt2tags.process_source_file(contents=txt)
+    data = txt2tags.process_source_file(contents=lines)
     # Then we convert it, dumping results to the 'tagged' list.
-    tagged, config = txt2tags.convert_this_files([data])
-    # Show the tagged file on the screen.
-    print '\n'.join(tagged)
-
-# Txt2tags error, show the message to the user
-except txt2tags.error, msg:
-    print msg
-    sys.exit(1)
-
-# Unknown error, show the traceback to the user
-except:
-    print txt2tags.getUnknownErrorMessage()
+    tagged_files, config = txt2tags.convert_this_files([data])
+    assert len(tagged_files) == 1, tagged_files
+    print '\n'.join(tagged_files[0])
+except txt2tags.error as err:
+    # Txt2tags error, show the message to the user
+    print err
     sys.exit(1)
