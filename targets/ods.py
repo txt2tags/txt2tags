93ea25733ded7a8d54c250d3eca4b2eaa0f9b4eb
Florent Gallaire <fgallaire@gmail.com>
2015-09-22 04:20:23 +0200

    ods: cell span support
    	modified:   targets/ods.py
    	modified:   txt2tags
    	modified:   txt2tagslite

--- targets/ods.py
+++ targets/ods.py
@@ -18,15 +18,17 @@ TAGS = {
     'tableClose'           : '</table:table>'                  ,
     'tableRowOpen'         : '<table:table-row>'               ,
     'tableRowClose'        : '</table:table-row>'              ,
-    'tableCellOpen'        : '<table:table-cell~A~><text:p>'   ,
+    'tableCellOpen'        : '<table:table-cell~A~~S~><text:p>'   ,
     'tableCellClose'       : '</text:p></table:table-cell>'    ,
-    'tableTitleCellOpen'   : '<table:table-cell~A~><text:p><text:span text:style-name="T1">',
+    'tableTitleCellOpen'   : '<table:table-cell~A~~S~><text:p><text:span text:style-name="T1">',
     'tableTitleCellClose'  : '</text:span></text:p></table:table-cell>',
+    'tableCellCovered'     : '<table:covered-table-cell table:number-columns-repeated="\a"/>' ,
     '_tableCellAlignCenter': ' table:style-name="ce1"',
     '_tableCellAlignRight' : ' table:style-name="ce2"',
     '_tableCellAlignLeftBorder'  : ' table:style-name="ce3"',
     '_tableCellAlignCenterBorder': ' table:style-name="ce4"',
     '_tableCellAlignRightBorder' : ' table:style-name="ce5"',
+    '_tableCellColSpan'    : ' table:number-columns-spanned="\a"',
     'EOD'                  : '</office:spreadsheet></office:body></office:document>',
 }
 
@@ -35,5 +37,7 @@ RULES = {
     'tableable': 1,
     'tableonly': 1,
     'tablecellstrip': 1,
+    'tablecellspannable': 1,
+    'tablecellcovered': 1,
     'tablecellaligntype': 'cell',
 }
